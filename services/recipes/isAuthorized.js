6e154a13ce69d04e99d0b5771b6ec2c097f76350
Julia Niestroj <jwylezo@gmail.com>
2021-11-04 13:26:40 +0100

    Add authorization for editing and deleting

--- services/recipes/isAuthorized.js
+++ services/recipes/isAuthorized.js
@@ -1,6 +1,6 @@
 const isAuthorized = (recipe, session) => {
   if (!session) return false;
-  if (session.user.id === recipe.users[0]) return true;
+  if (session.user.id === recipe.email[0]) return true;
 
   return false;
 };
